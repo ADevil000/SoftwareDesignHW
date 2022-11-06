b9bab817481e2faadf58d8020f1cccd0438da95f
Artem Kolosov <ar.kolosov@vk.team>
2022-11-06 13:34:56 +0300

    Refactoring QueryServlet: remove code duplicates, add helper enum

--- Lab3/src/main/java/ru/akirakozov/sd/refactoring/servlet/QueryServlet.java
+++ Lab3/src/main/java/ru/akirakozov/sd/refactoring/servlet/QueryServlet.java
@@ -7,6 +7,7 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.io.PrintWriter;
 import java.util.List;
 import java.util.Map;
 
@@ -14,6 +15,9 @@ import java.util.Map;
  * @author akirakozov
  */
 public class QueryServlet extends HttpServlet {
+    private enum Action {
+        MAX, MIN, SUM, COUNT, UNKNOWN
+    }
     public final ProductDatabase database;
     public static final int NEUTRAL = Integer.MIN_VALUE;
 
@@ -27,57 +31,83 @@ public class QueryServlet extends HttpServlet {
     @Override
     public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
         String command = request.getParameter("command");
+        Action action;
+        try {
+            String rawValue = command.toUpperCase();
+            action = Action.valueOf(rawValue);
+        } catch (Exception ignored) {
+            action = Action.UNKNOWN;
+        }
+        PrintWriter writer = response.getWriter();
 
-        if ("max".equals(command)) {
-            String query = "SELECT * FROM PRODUCT ORDER BY PRICE DESC LIMIT 1";
-            List<Map<String, String>> result = database.getNamesAndPrices(query);
-
-            response.getWriter().println("<html><body>");
-            response.getWriter().println("<h1>Product with max price: </h1>");
-            for (Map<String, String> pairs : result) {
-                String name = pairs.get("name");
-                String price  = pairs.get("price");
-                response.getWriter().println(name + "\t" + price + "</br>");
-            }
-            response.getWriter().println("</body></html>");
-        } else if ("min".equals(command)) {
-            String query = "SELECT * FROM PRODUCT ORDER BY PRICE LIMIT 1";
-            List<Map<String, String>> result = database.getNamesAndPrices(query);
-
-            response.getWriter().println("<html><body>");
-            response.getWriter().println("<h1>Product with min price: </h1>");
-            for (Map<String, String> pairs : result) {
-                String name = pairs.get("name");
-                String price  = pairs.get("price");
-                response.getWriter().println(name + "\t" + price + "</br>");
-            }
-            response.getWriter().println("</body></html>");
-        } else if ("sum".equals(command)) {
-            String query = "SELECT SUM(price) FROM PRODUCT";
-            int result = database.getScalarNumber(query, NEUTRAL);
-
-            response.getWriter().println("<html><body>");
-            response.getWriter().println("Summary price: ");
-            if (result != NEUTRAL) {
-                response.getWriter().println(result);
-            }
-            response.getWriter().println("</body></html>");
-        } else if ("count".equals(command)) {
-            String query = "SELECT COUNT(*) FROM PRODUCT";
-            int result = database.getScalarNumber(query, NEUTRAL);
-
-            response.getWriter().println("<html><body>");
-            response.getWriter().println("Number of products: ");
-            if (result != NEUTRAL) {
-                response.getWriter().println(result);
-            }
-            response.getWriter().println("</body></html>");
-        } else {
-            response.getWriter().println("Unknown command: " + command);
+        switch (action) {
+            case MAX:
+                max(writer);
+                break;
+            case MIN:
+                min(writer);
+                break;
+            case SUM:
+                sum(writer);
+                break;
+            case COUNT:
+                count(writer);
+                break;
+            case UNKNOWN:
+                writer.println("Unknown command: " + command);
+                break;
         }
 
         response.setContentType("text/html");
         response.setStatus(HttpServletResponse.SC_OK);
+    }
+
+    private void max(PrintWriter writer) {
+        String query = "SELECT * FROM PRODUCT ORDER BY PRICE DESC LIMIT 1";
+        String info = "<h1>Product with max price: </h1>";
+        doSortQuery(writer, query, info);
+    }
+
+    private void min(PrintWriter writer) {
+        String query = "SELECT * FROM PRODUCT ORDER BY PRICE LIMIT 1";
+        String info = "<h1>Product with min price: </h1>";
+        doSortQuery(writer, query, info);
+    }
+
+    private void doSortQuery(PrintWriter writer, String query, String info) {
+        List<Map<String, String>> result = database.getNamesAndPrices(query);
+
+        writer.println("<html><body>");
+        writer.println(info);
+        for (Map<String, String> pairs : result) {
+            String name = pairs.get("name");
+            String price  = pairs.get("price");
+            writer.println(name + "\t" + price + "</br>");
+        }
+        writer.println("</body></html>");
+    }
+
+    private void sum(PrintWriter writer) {
+        String query = "SELECT SUM(price) FROM PRODUCT";
+        String info = "Summary price: ";
+        doScalarQuery(writer, query, info);
+    }
+
+    private void count(PrintWriter writer) {
+        String query = "SELECT COUNT(*) FROM PRODUCT";
+        String info = "Number of products: ";
+        doScalarQuery(writer, query, info);
+    }
+
+    private void doScalarQuery(PrintWriter writer, String query, String info) {
+        int result = database.getScalarNumber(query, NEUTRAL);
+
+        writer.println("<html><body>");
+        writer.println(info);
+        if (result != NEUTRAL) {
+            writer.println(result);
+        }
+        writer.println("</body></html>");
     }
 
 }
